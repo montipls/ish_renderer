c9bb053e377d3a7d269ba934a448e8959c4eb449
Monti <montikoszo@gmail.com>
2025-06-06 22:45:41 +0200

    Added main loop, testing required...

--- test.py
+++ test.py
@@ -1,19 +1,43 @@
 from PIL import Image
 from renderer import *
 import shutil
+import curses
 
 
-x, _ = shutil.get_terminal_size()
-even = x % 2 == 0
-print(x)
+W, H = shutil.get_terminal_size()
+even = W % 2 == 0
 
 bg_img = Image.open('bg.jpeg')
-bg_img = bg_img.resize((x, x if even else x-1))
+bg_img = bg_img.resize((W, W if even else W-1))
 window = load_sprite(bg_img)
 
 sprite_img = Image.open('sprite.png')
 sprite = load_sprite(sprite_img)
 blit_sprite(window, sprite, 10, 10)
-
 frame = get_string(window)
-print('\n'.join(frame))
+W = len(frame[0])
+H = len(frame)
+
+
+def main(stdscr):
+    curses.curs_set(0)
+    stdscr.nodelay(True)
+
+    timer = 0
+    while True:
+        timer += 1
+
+        blit_sprite(window, sprite, 10, timer)
+        frame = get_string(window)
+
+        for y, line in enumerate(frame):
+            stdscr.addstr(y, 0, line)
+
+        stdscr.addstr(H, 0, f"window size: {W} : {H}")
+        stdscr.refresh()
+
+        if stdscr.getch() == ord('q'):
+            break
+
+
+curses.wrapper(main)
