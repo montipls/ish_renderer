cd7a3d52c552c5262ce21daed1b72bb6817c37aa
Monti <montikoszo@gmail.com>
2025-06-06 22:16:27 +0200

    dynamic window size :D

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from PIL import Image
 from renderer import *
+import shutil
 
+
+_, rows = shutil.get_terminal_size()
 
 img = Image.open('bg.jpeg')
-img = img.resize((68, 68))
+img = img.resize((rows, rows))
 window = load_sprite(img)
 
 sprite = Image.open('sprite.jpeg')
