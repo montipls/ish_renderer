be87dbf317c5f7939d86e29011f24b876b750972
Monti <montikoszo@gmail.com>
2025-06-07 00:03:36 +0200

    final optimizations

--- test.py
+++ test.py
@@ -23,6 +23,13 @@ def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(True)
 
+    write = sys.stdout.write  # local alias for speed
+    flush = sys.stdout.flush
+    
+    esc_home = "\033[H"
+    esc_reset = "\033[0m"
+    info = f"{esc_reset}window size: {W} : {H}\n"
+
     timer = 0
     while True:
         timer += 1
@@ -32,10 +39,10 @@ def main(stdscr):
 
         frame = get_string(blit_sprite(window, sprite, 10, timer))
 
-        sys.stdout.write("\033[H")  # move cursor to top-left
-        sys.stdout.write(''.join(frame))
-        sys.stdout.write(f"\033[0mwindow size: {W} : {H}\n")
-        sys.stdout.flush()
+        write(esc_home)
+        write(''.join(frame))
+        write(info)
+        flush()
 
         stdscr.refresh()
 
